8bf73b0f41d0ac11ec0b855e04889694a3758dad
Ángel Martín <79909511+angmarnun1@users.noreply.github.com>
2022-01-14 14:24:28 +0100

    Examen-Hasta-Test7
    Examen realizado por angmarnun1 hasta el test7

--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccination.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccination.java
@@ -2,19 +2,40 @@ package org.springframework.samples.petclinic.vacination;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotNull;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.Pet;
+
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Vaccination {
-    Integer id;
-    LocalDate date;
-    Pet vaccinatedPet;   
-    @Transient
-    Vaccine vaccine; 
+@Entity
+@Table(name = "vaccinations")
+public class Vaccination extends BaseEntity {
+   
+	@NotNull
+	@JoinColumn(name = "date")        
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
+    private LocalDate date;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "vaccinated_pet_id")
+    private Pet vaccinatedPet;  
+    
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "vaccine_id")
+    private Vaccine vaccine;
 }
--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccine.java
@@ -1,5 +1,15 @@
 package org.springframework.samples.petclinic.vacination;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +17,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class Vaccine {
-    Integer id;
-    String name;
-    Double price;
+@Entity
+@Table(name = "vaccines")
+public class Vaccine extends BaseEntity{
+	
+	
+	@NotNull
+	@Length(min = 3, max = 50)
+	@JoinColumn(name = "name", unique = true)
+    private String name;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "pet_type_id")
     PetType petType;
+	
+	@NotNull
+	@Min(0)
+	@JoinColumn(name = "price")
+    private Double price;
+	
+
 }
